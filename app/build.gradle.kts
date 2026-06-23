4d82216f1946a2c7e051c8caf11451cd08d9ead2
Oliver29Klozoff <your-github-digler229@gmail.com>
2026-06-23 14:22:11 -0400

    Bump version to 2.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.iptvnative"
         minSdk = 35
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 42
+        versionName = "2.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
